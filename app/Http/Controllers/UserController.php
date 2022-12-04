7fc28cd95b2049bd70b3eb2e14b02dbbf5df4f98
Mohammad Ricky Susanto <echo.mricky.it@gmail.com>
2022-12-05 00:43:38 +0700

    implementation middleware

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Services\Impl\UserServiceImpl;
+use App\Services\UserService;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class UserController extends Controller
 {
@@ -13,8 +16,42 @@ class UserController extends Controller
     {
         $this->userService = $userService;
     }
-    public function login(string $user, string $password){
+    public function login(): Response {
         
-        $test = $this->userService($user,$password);
+        return response()
+            ->view('user.login',[
+                'title' => 'login'
+            ]);
+        //$test = $this->userService($user,$password);
+    }
+
+    public function doLogin(Request $request) {
+        $user = $request->input('user');
+        $password = $request->input('password');
+
+        // validate move forwad using validate laravel
+        if (empty($user) || empty($password)){
+            return response()->view('user.login',[
+                'title' => 'Login',
+                'error' => 'User or Password is required'
+            ]);
+        }
+
+        if($this->userService->login($user,$password)){
+            $request->session()->put('user',$user);
+
+            return redirect("/");
+        }
+        return response()->view('user.login',[
+            'title' => "Login",
+            "error" => "User or password wrong"
+        ]);
+    }
+
+    public function doLogout(Request $request){
+        
+        $request->session()->forget("user");
+
+        return redirect("/");
     }
 }
